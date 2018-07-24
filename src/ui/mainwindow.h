1bbedbaf2a6eaa40e875e6ba59f367e1f2f5ec74
Jalen Adams <leftysolara@gmail.com>
2018-07-24 17:16:42 -0400

    add license broilerplate

--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -1,3 +1,26 @@
+/******************************************************************************
+ * mainwindow.h : functions for managing the main application window
+ * ****************************************************************************
+ * Copyright (C) 2018 Jalen Adams
+ *
+ * Authors: Jalen Adams <leftysolara@gmail.com>
+ *
+ * This file is part of kanban.
+ *
+ * kanban is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kanban is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kanban.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -17,7 +40,6 @@ public:
 
 private slots:
     void on_actionQuit_triggered();
-
     void on_actionAboutQt_triggered();
 
 private:
